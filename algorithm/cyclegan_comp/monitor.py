262dac3964af63b5d72223e7eb7cde7bae1fb2d8
abhishtagatya <abhishtagatya@yahoo.com>
2022-06-12 01:09:45 +0700

    Training CycleGAN : - Fix DisplayMonitor on CycleGAN #3

--- algorithm/cyclegan_comp/monitor.py
+++ algorithm/cyclegan_comp/monitor.py
@@ -19,30 +19,34 @@ class CheckpointMonitor(tf.keras.callbacks.Callback):
 
 class DisplayMonitor(tf.keras.callbacks.Callback):
 
-    def __init__(self, model_name, domain, dataset):
+    def __init__(self, model_name, domain, dataset, image_size=(256, 26, 3)):
         self.model_name = model_name
         self.domain = domain
         self.dataset = dataset
+        self.image_size = image_size
 
-        self.select_image = self.dataset.take(1).get_single_element()[0]
+        self.content, self.style = self.select_image = self.dataset.take(1).get_single_element()
 
     def on_epoch_end(self, epoch, logs=None):
-        _, ax = plt.subplots(1, 2, figsize=(12, 12))
+        _, ax = plt.subplots(1, 3, figsize=(12, 12))
 
         # Create the img
-        for i, img in enumerate(self.select_image):
-            pred = self.model.gen_G(img)[0].numpy()
-            pred = (pred * 127.5 + 127.5).astype(np.uint8)
-            img = (img[0] * 127.5 + 127.5).numpy().astype(np.uint8)
+        pred = self.model.gen_G(self.content)[0].numpy()
+        pred = (pred * 127.5 + 127.5).astype(np.uint8)
+        img = (self.content[0] * 127.5 + 127.5).numpy().astype(np.uint8)
+        img_b = (self.style[0] * 127.5 + 127.5).numpy().astype(np.uint8)
 
-            # Plot the style, content, image
-            ax[0].imshow(img)
-            ax[0].set_title(f"Input: {epoch + 1:03d}")
+        # Plot the style, content, image
+        ax[0].imshow(img_b)
+        ax[0].set_title(f"Style: {epoch + 1:03d}")
 
-            ax[1].imshow(pred)
-            ax[1].set_title(f"Translated: {epoch + 1:03d}")
+        ax[1].imshow(img)
+        ax[1].set_title(f"Content: {epoch + 1:03d}")
 
-            plt.savefig(f'{self.model_name}/model_results/{self.model_name}-{self.domain}_{epoch + 1}.png', format='png')
+        ax[2].imshow(pred)
+        ax[2].set_title(f"Translated: {epoch + 1:03d}")
 
-            plt.show()
-            plt.close()
+        plt.savefig(f'{self.model_name}/model_results/{self.model_name}-{self.domain}_{epoch + 1}.png', format='png')
+
+        plt.show()
+        plt.close()
