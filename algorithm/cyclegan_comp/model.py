f6d447eae4c0a0b0f2b7089a6d651c1b4a15ac59
abhishtagatya <abhishtagatya@yahoo.com>
2022-06-16 00:29:38 +0700

    CycleGAN Checkpoint : - Changed CycleGAN Checkpoint Path

--- algorithm/cyclegan_comp/model.py
+++ algorithm/cyclegan_comp/model.py
@@ -241,9 +241,8 @@ class CycleGenAdvNet(tf.keras.Model):
             "D_Y_loss": disc_Y_loss
         }
 
-    def restore_checkpoint(self):
-        if self.checkpoint_manager.latest_checkpoint:
-            self.model_checkpoint.restore(self.checkpoint_manager.latest_checkpoint)
+    def restore_checkpoint(self, checkpoint_path):
+        self.model_checkpoint.restore(checkpoint_path)
 
     def inference(self, content):
         pred = self.gen_G(content)[0].numpy()
